08592a7a35b7018b2594868b5e32918fb521114d
Yordin <yycruzr@unitru.edu.pe>
2025-10-30 10:22:22 -0500

    Gravedad jugador completado

--- core/src/main/java/com/yordin/jungle_leap/entidades/Jugador.java
+++ core/src/main/java/com/yordin/jungle_leap/entidades/Jugador.java
@@ -7,13 +7,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.yordin.jungle_leap.recursos.Texturas;
+import com.yordin.jungle_leap.utils.Constantes;
 
 
 public class Jugador {
+    enum Estado{
+        SALTANDO,
+        CORRIENDO,
+        INACTIVO,
+        HERIDO,
+        DESLIZANDO,
+        MURIENDO
+    }
+
     private Texture texture;
     private Vector2 posicion;
     private Vector2 velocidad;
     private float velocidadModulo;
+    private float fuerzaSalto;
+    private Estado estadoActual = Estado.INACTIVO;
+    private boolean enPiso = false;
     private Rectangle hitBox;
 
     public Jugador(float x, float y){
@@ -21,11 +34,16 @@ public class Jugador {
         posicion = new Vector2(x,y);
         velocidad = new Vector2(0,0);
         velocidadModulo = 150;
+        fuerzaSalto = 30;
         hitBox = new Rectangle(posicion.x, posicion.y, texture.getWidth(), texture.getHeight());
     }
 
     public void update(float delta){
         posicion.mulAdd(velocidad, delta);
+
+        //GRAVEDAD
+        if(enPiso) velocidad.y = 0;
+        else velocidad.y += Constantes.gravedad;
     }
     public void mover(){
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) velocidad.x = velocidadModulo;
---
+++ core/src/main/java/com/yordin/jungle_leap/utils/Constantes.java
@@ -0,0 +1,6 @@
+package com.yordin.jungle_leap.utils;
+
+public class Constantes {
+    public static final float gravedad = -20f;
+
+}
